9c3b037605a8900425fb176e4ec1881d4a0a9193
Cagatay Dogan <ocdogan@gmail.com>
2016-11-06 19:07:33 +0300

    Formatted

--- Sweet.Jayson.Tests/DefaultValueOverrideAttribute.cs
+++ Sweet.Jayson.Tests/DefaultValueOverrideAttribute.cs
@@ -3,21 +3,21 @@ using System.ComponentModel;
 
 namespace Sweet.Jayson.Tests
 {
-	public class DefaultValueOverrideAttribute : DefaultValueAttribute
-	{
-        public DefaultValueOverrideAttribute (Type valueType)
-			: base(null)
-		{
-			if (valueType != null) 
-			{
-				try 
-				{
-					base.SetValue(Activator.CreateInstance (valueType, true));
-				} 
-				catch (Exception) 
-				{ }
-			}
-		}
-	}
+    public class DefaultValueOverrideAttribute : DefaultValueAttribute
+    {
+        public DefaultValueOverrideAttribute(Type valueType)
+            : base(null)
+        {
+            if (valueType != null)
+            {
+                try
+                {
+                    base.SetValue(Activator.CreateInstance(valueType, true));
+                }
+                catch (Exception)
+                { }
+            }
+        }
+    }
 }
 
